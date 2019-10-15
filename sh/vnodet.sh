7d2ac5485374a72a00336aea37ab21daa7137da0
rgb-24bit <rgb-24bit@foxmail.com>
2019-10-15 09:47:56 +0800

    Update the vpn node test script

--- sh/vnodet.sh
+++ sh/vnodet.sh
@@ -4,17 +4,34 @@
 
 SERVER_NAMES=""
 SERVER_DOMAIN=""
+COUNT=1
+TIMEOUT=1
+
+while getopts "c:t:" arg
+do
+    case $arg in
+        c)
+            COUNT=$OPTARG
+            ;;
+        t)
+            TIMEOUT=$OPTARG
+            ;;
+        ?)
+        ;;
+    esac
+done
 
 for SERVER_NAME in $SERVER_NAMES
 do
     SERVER=$SERVER_NAME.$SERVER_DOMAIN
 
-    TEST=`ping -c 1 -t 1 $SERVER | grep round-trip`
+    TEST=`ping -c $COUNT -t $TIMEOUT $SERVER | grep round-trip`
 
     if [ -z "$TEST" ]; then
+        # Faild with red color
         echo -e "\033[31m $SERVER \033[0m"
     else
-        echo -e "\033[32m $SERVER \033[0m"
+        echo -e "\033[32m $SERVER -> $TEST \033[0m"
     fi
 done
 
